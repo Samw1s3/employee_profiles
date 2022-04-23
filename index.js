271d28a76f0901c16ddbf383afabbf5d73b4f963
Sam Knopper <samanthaknopper@hotmail.com>
2022-04-23 18:19:30 +0800

    Input questions completed for manager

--- index.js
+++ index.js
@@ -10,8 +10,30 @@ const promptUser = () => {
         },
         {
             type: 'input',
-            name: 'name',
+            name: 'managerId',
             message: "What is the team member's employee ID?" 
         },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email address?",
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number?", 
+        },
+        {
+            type: 'list',
+            name: 'employeeType',
+            message: 'What type of employee information would you like to enter?',
+            choices: ['engineer', 'intern'],
+        },
+        if (employeeType === 'engineer'){
+            [
+                
+            ]
+        }
     ])
+    
 }
